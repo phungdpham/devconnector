29de8049351c5e8bd940813e9d4b02194aeac449
Phung Pham <phung.tech@gmail.com>
2021-03-24 15:21:18 -0500

    in process

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,27 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import { REGISTER_SUCCESS, REGISTER_FAIL, AUTH_ERROR, USER_LOADED } from "./types";
+
+// Load User
+export const loadUser = () => async dispatch => {
+  if(localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/api/auth')
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    })
+  }
+}
 
 // Register User
 export const register = ({ name, email, password }) => async (dispatch) => {
